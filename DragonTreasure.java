b77ac3ccea57c76ae1ede8cbbc8df86a8c1c6255
magfer3 <45487344+magfer3@users.noreply.github.com>
2018-12-13 14:10:58 +0100

    Add files via upload

--- DragonTreasure.java
+++ DragonTreasure.java
@@ -27,18 +27,41 @@ public class DragonTreasure {
         //Initiates a new player
         player player = new player(input.nextLine()); 
         
+        //Game starts
         System.out.printf("\nWelcome %s to your treasure hunt. \n"
                 + "Beware of the dragon! You are standing outside a cave. \n"
-                + "There is a smell of sulfur coming from the opening" 
+                + "There is a smell of sulfur coming from the opening. " 
                 + "The cave opening is to your east. \n"
-                + "Write \"e\" and press [Enter] to enter the cave", player.getName());
+                + "Write \"e\" and press [Enter] to enter the cave: \n\n", player.getName());
         
-        
-        
+
         //Continue game until players bodypoint reaches 0 or less
         while(player.getBodypoint() > 0){
         
+        while(!input.nextLine().equals("e")){
+            System.out.println("You cant go anywhere else. Do you want to enter? "
+                    + "[y] / [n] ?\n");
+            if(input.nextLine().equals("y")){
+                break;  
+            }
+        }
             
+        System.out.println("\nAs you enter the cave the entrance "
+            + "collapses behind you.\n" 
+            + "The room is lit by a few candles sitting on a "
+            + "table in front of you.\n" 
+            + "You can go north [n] and south [s]\n\n");
+        
+        /*switch (input.nextLine()){
+            case "n": 
+                System.out.println("\nYou see a sword next to a dead body "
+                        + "on the floor.\n" 
+                        + "You can pick up the sword [p], and go [s] or [e]");
+            case "s": ;
+                System.out.println("\nYou see a key laying on the floor.\n"
+                        + "You can pick up the key [p], and go [w] or [e]");
+        }*/
+ 
             
         }
         
